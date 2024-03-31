cae7e54e4c258d97ce73c87d619439625cb9a91a
sylvaindl <sylvaindelange@hotmail.com>
2024-03-31 13:37:51 +0200

    toevoegen

---
+++ mijnvenv/Lib/site-packages/polls/views.py
@@ -0,0 +1,133 @@
+# _*_ coding:utf-8 _*_
+
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
+from django.db.models import F
+from django.views import generic
+from django.utils import timezone
+
+from .models import Question, Choice
+
+
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     output = ', '.join([q.question_text for q in latest_question_list])
+#     return HttpResponse(output)
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+## shortcut
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+## use generic view
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        # Return the last five published questions.
+        # return Question.objects.order_by('-pub_date')[:5]
+
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+
+
+
+
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', {'question': question})
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+## use generic view
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    # the default context_object_name is 'question'
+    # context_object_name = 'question'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+
+
+
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+## use generic view
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+    # the default context_object_name is 'question'
+    # context_object_name = 'question'
+
+
+
+# def vote(request, question_id):
+#     return HttpResponse("You're voting on question %s." % question_id)
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # 重新展示detail表格
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        # 只要处理成功，就返回HttpResponseRedirect 重定向
+        # 使用POST提交，该方法避免用户使用浏览器的回退按钮进行两次投票
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+
+
+
+
+
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -9,6 +9,10 @@ class Post(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
@@ -16,4 +20,6 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def __str__(self):
+        return self.question_text
 
